f52686906d4ed6d7bd0301d9cc77234c94d4cc0b
BT_2N2_06 <sample@gmail.com>
2024-03-19 13:44:55 -0400

    done

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class Rectangle {
+
+public class Rectangle extends Polygon {
+    private int length;
+    private int width;
+
+    public Rectangle(int length, int width) {
+        super(4);
+        this.length = length;
+        this.width = width;
+    }
+
+    public int area() {
+        return length * width;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", length = " + length + ", width = " + width;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof Rectangle)) {
+            return false;
+        }
+
+        Rectangle objRect = (Rectangle) obj;
+
+        return (super.equals(objRect) &&
+                length == objRect.length &&
+                width == objRect.width);
+    }
 }
--- src/ThreeDPoint.java
+++ src/ThreeDPoint.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class ThreeDPoint {
+public class ThreeDPoint extends Point{
+    private int z;
+    public ThreeDPoint(String label, int x, int y, int z) {
+        super(label, x, y);
+        this.z = z;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", z = " + z;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof  ThreeDPoint)) {
+            return false;
+        }
+        ThreeDPoint point = (ThreeDPoint) obj;
+        if (super.equals(obj)) {
+            return z == point.z;
+        } else {
+            return false;
+        }
+    }
 }
